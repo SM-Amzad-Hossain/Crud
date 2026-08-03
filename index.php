a9550f245fc303cbb66dc3281decf72d05bf8a4f
0amzad77 <0amzad77@gmail.com>
2026-08-03 23:35:42 +0600

    Update Students Data other method

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use LDAP\Result;
+
 require_once "inc/function.php";
 $info = '';
 $task = $_GET['task'] ?? 'report';
@@ -17,7 +19,6 @@ if (isset($_POST['submit'])) {
     $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_SPECIAL_CHARS);
     $roll = filter_input(INPUT_POST, 'roll', FILTER_SANITIZE_SPECIAL_CHARS);
     $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
-
     if ($id) {
         if ($fname != '' && $lname != '' && $roll != '') {
             $result = updateStudent($id, $fname, $lname, $roll);
@@ -86,13 +87,13 @@ if (isset($_POST['submit'])) {
         <?php if ('add' == $task): ?>
             <div class="row">
                 <div class="column column-60 column-offset-20">
-                    <form action="" method="POST">
+                    <form action="index.php?task=add" method="POST">
                         <label for="fname">First Name</label>
-                        <input type="text" name="fname" id="fname" value="<?php echo htmlspecialchars($fname); ?>">
+                        <input type="text" name="fname" id="fname" value="<?php echo $fname; ?>">
                         <label for="lname">Last Name</label>
-                        <input type="text" name="lname" id="lname" value="<?php echo htmlspecialchars($lname); ?>">
+                        <input type="text" name="lname" id="lname" value="<?php echo $lname; ?>">
                         <label for="roll">Roll</label>
-                        <input type="text" name="roll" id="roll" value="<?php echo htmlspecialchars($roll); ?>">
+                        <input type="text" name="roll" id="roll" value="<?php echo $roll; ?>">
                         <button type="submit" class="button-primary" value="Save" name="submit">Save</button>
                     </form>
                 </div>
@@ -100,11 +101,8 @@ if (isset($_POST['submit'])) {
         <?php endif; ?>
         <?php
         if ('edit' == $task):
-            $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
-            if (!$id) {
-                $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-            $student = getStudent((int)$id);
+            $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+            $student = getStudent($id);
             if ($student):
         ?>
                 <div class="row">
@@ -112,19 +110,20 @@ if (isset($_POST['submit'])) {
                         <form action="index.php?task=edit&id=<?php echo $id; ?>" method="POST">
                             <input type="hidden" name="id" value="<?php echo $id; ?>">
                             <label for="fname">First Name</label>
-                            <input type="text" name="fname" id="fname" value="<?php echo htmlspecialchars($student['fname']); ?>">
+                            <input type="text" name="fname" id="fname" value="<?php echo $student['fname']; ?>">
                             <label for="lname">Last Name</label>
-                            <input type="text" name="lname" id="lname" value="<?php echo htmlspecialchars($student['lname']); ?>">
+                            <input type="text" name="lname" id="lname" value="<?php echo $student['lname']; ?>">
                             <label for="roll">Roll</label>
-                            <input type="text" name="roll" id="roll" value="<?php echo htmlspecialchars($student['roll']); ?>">
+                            <input type="text" name="roll" id="roll" value="<?php echo $student['roll']; ?>">
                             <button type="submit" class="button-primary" name="submit">Update</button>
                         </form>
                     </div>
                 </div>
         <?php endif;
         endif; ?>
+        ?>
 
-            </div>
-        </body>
+    </div>
+</body>
 
-        </html>
+</html>
